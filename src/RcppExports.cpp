3ee43b4d7d1d36a554755167563dcd4796fd0695
Jingyu He <jingyuhe@github.com>
2018-04-29 20:54:39 -0500

    add adaptive sampling

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -37,6 +37,34 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// train_forest_adaptive
+Rcpp::List train_forest_adaptive(arma::mat y, arma::mat X, arma::mat Xtest, size_t M, size_t L, size_t N_sweeps, arma::mat max_depth, size_t Ncutpoints, size_t Nmin, double alpha, double beta, double tau, bool draw_sigma, double kap, double s, bool verbose, bool m_update_sigma, bool draw_mu);
+RcppExport SEXP _SpikySmoothTrees_train_forest_adaptive(SEXP ySEXP, SEXP XSEXP, SEXP XtestSEXP, SEXP MSEXP, SEXP LSEXP, SEXP N_sweepsSEXP, SEXP max_depthSEXP, SEXP NcutpointsSEXP, SEXP NminSEXP, SEXP alphaSEXP, SEXP betaSEXP, SEXP tauSEXP, SEXP draw_sigmaSEXP, SEXP kapSEXP, SEXP sSEXP, SEXP verboseSEXP, SEXP m_update_sigmaSEXP, SEXP draw_muSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Xtest(XtestSEXP);
+    Rcpp::traits::input_parameter< size_t >::type M(MSEXP);
+    Rcpp::traits::input_parameter< size_t >::type L(LSEXP);
+    Rcpp::traits::input_parameter< size_t >::type N_sweeps(N_sweepsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type max_depth(max_depthSEXP);
+    Rcpp::traits::input_parameter< size_t >::type Ncutpoints(NcutpointsSEXP);
+    Rcpp::traits::input_parameter< size_t >::type Nmin(NminSEXP);
+    Rcpp::traits::input_parameter< double >::type alpha(alphaSEXP);
+    Rcpp::traits::input_parameter< double >::type beta(betaSEXP);
+    Rcpp::traits::input_parameter< double >::type tau(tauSEXP);
+    Rcpp::traits::input_parameter< bool >::type draw_sigma(draw_sigmaSEXP);
+    Rcpp::traits::input_parameter< double >::type kap(kapSEXP);
+    Rcpp::traits::input_parameter< double >::type s(sSEXP);
+    Rcpp::traits::input_parameter< bool >::type verbose(verboseSEXP);
+    Rcpp::traits::input_parameter< bool >::type m_update_sigma(m_update_sigmaSEXP);
+    Rcpp::traits::input_parameter< bool >::type draw_mu(draw_muSEXP);
+    rcpp_result_gen = Rcpp::wrap(train_forest_adaptive(y, X, Xtest, M, L, N_sweeps, max_depth, Ncutpoints, Nmin, alpha, beta, tau, draw_sigma, kap, s, verbose, m_update_sigma, draw_mu));
+    return rcpp_result_gen;
+END_RCPP
+}
 // train_forest_std
 Rcpp::List train_forest_std(Rcpp::NumericMatrix y_rcpp, Rcpp::NumericMatrix X_rcpp, Rcpp::NumericMatrix Xtest_rcpp, Rcpp::IntegerMatrix Xorder_rcpp, size_t M, size_t L, size_t N_sweeps, Rcpp::NumericMatrix max_depth_rcpp, size_t Nmin, double alpha, double beta, double tau, bool draw_sigma, double kap, double s, bool verbose, bool m_update_sigma, bool draw_mu);
 RcppExport SEXP _SpikySmoothTrees_train_forest_std(SEXP y_rcppSEXP, SEXP X_rcppSEXP, SEXP Xtest_rcppSEXP, SEXP Xorder_rcppSEXP, SEXP MSEXP, SEXP LSEXP, SEXP N_sweepsSEXP, SEXP max_depth_rcppSEXP, SEXP NminSEXP, SEXP alphaSEXP, SEXP betaSEXP, SEXP tauSEXP, SEXP draw_sigmaSEXP, SEXP kapSEXP, SEXP sSEXP, SEXP verboseSEXP, SEXP m_update_sigmaSEXP, SEXP draw_muSEXP) {
@@ -96,6 +124,7 @@ END_RCPP
 static const R_CallMethodDef CallEntries[] = {
     {"_SpikySmoothTrees_predict_tree", (DL_FUNC) &_SpikySmoothTrees_predict_tree, 2},
     {"_SpikySmoothTrees_singletree", (DL_FUNC) &_SpikySmoothTrees_singletree, 9},
+    {"_SpikySmoothTrees_train_forest_adaptive", (DL_FUNC) &_SpikySmoothTrees_train_forest_adaptive, 18},
     {"_SpikySmoothTrees_train_forest_std", (DL_FUNC) &_SpikySmoothTrees_train_forest_std, 18},
     {"_SpikySmoothTrees_train_forest", (DL_FUNC) &_SpikySmoothTrees_train_forest, 17},
     {NULL, NULL, 0}
--- src/tree.h
+++ src/tree.h
@@ -35,6 +35,26 @@
 
 
 //--------------------------------------------------
+//find best split variable and value, CART
+void split_error(const arma::umat& Xorder, arma::vec& y, arma::uvec& best_split, arma::vec& least_error);
+void split_xorder_std(xinfo_sizet& Xorder_left, xinfo_sizet& Xorder_right, xinfo_sizet& Xorder, double *  X, size_t split_var, size_t split_point, size_t N, size_t p);
+void BART_likelihood(const arma::umat& Xorder, arma::vec& y, double tau, double sigma, size_t depth, size_t Nmin, double alpha, double beta, size_t& ind, size_t& split_var, size_t& split_point);
+void BART_likelihood_adaptive(const arma::umat& Xorder, arma::vec& y, arma::vec& loglike, double tau, double sigma, size_t depth, size_t Nmin, size_t Ncutpoints, double alpha, double beta, size_t& ind, size_t& split_var, size_t& split_point);
+void BART_likelihood_std(size_t N, size_t p, xinfo_sizet& Xorder, double* y, std::vector<double>& loglike, double& tau, double& sigma, size_t& depth, double& alpha, double& beta);
+void cumulative_sum_std(std::vector<double>& y_cumsum, std::vector<double>& y_cumsum_inv, double& y_sum, double* y, xinfo_sizet& Xorder, size_t& i, size_t& N);
+//--------------------------------------------------
+//split Xorder matrix for two subnodes 
+void split_xorder(arma::umat& Xorder_left, arma::umat& Xorder_right, arma::umat& Xorder, arma::mat& X, size_t split_var, size_t split_point);
+//--------------------------------------------------
+//BART likelihood function
+arma::vec BART_likelihood_function(arma::vec& n1, arma::vec& n2, arma::vec& s1, arma::vec& s2, double& tau, double& sigma, double& alpha, double& penalty);
+//--------------------------------------------------
+//generate a vector of integers
+arma::uvec range(size_t start, size_t end);
+
+
+
+//--------------------------------------------------
 //info contained in a node, used by input operator
 struct node_info {
    std::size_t id; //node id
@@ -92,7 +112,7 @@ public:
    tree_p gettop(); // get pointer to the top node
   //  void grow_tree(arma::vec& y, double y_mean, arma::umat& Xorder, arma::mat& X, size_t depth, size_t max_depth, size_t Nmin, double tau, double sigma, double alpha, double beta);
    void grow_tree(arma::vec& y, double y_mean, arma::umat& Xorder, arma::mat& X, size_t depth, size_t max_depth, size_t Nmin, double tau, double sigma, double alpha, double beta, arma::vec& residual, bool draw_sigma, bool draw_mu);
-   void grow_tree_adaptive(arma::vec& y, double y_mean, arma::umat& Xorder, arma::mat& X, size_t depth, size_t max_depth, size_t Nmin, double tau, double sigma, double alpha, double beta, arma::vec& residual, bool draw_sigma, bool draw_mu);
+   void grow_tree_adaptive(arma::vec& y, double y_mean, arma::umat& Xorder, arma::mat& X, size_t depth, size_t max_depth, size_t Nmin, size_t Ncutpoints, double tau, double sigma, double alpha, double beta, arma::vec& residual, bool draw_sigma, bool draw_mu);
    void grow_tree_std(double* y, double& y_mean, xinfo_sizet& Xorder, double* X, size_t N, size_t p, size_t depth, size_t max_depth, size_t Nmin, double tau, double sigma, double alpha, double beta, double* residual, bool draw_sigma, bool draw_mu);
    tree_p bn(double *x,xinfo& xi); //find Bottom Node, original BART version
    tree_p bn_std(double *x); // find Bottom Node, std version, compare
