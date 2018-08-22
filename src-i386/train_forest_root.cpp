98172ea25d518c6fc5a373933c0e97f2ac873c54
Jingyu He <jingyuhe@hotmail.com>
2018-08-22 16:52:27 -0500

    “i386

---
+++ src-i386/train_forest_root.cpp
@@ -0,0 +1,217 @@
+#include <ctime>
+#include <RcppArmadillo.h>
+#include "tree.h"
+#include "treefuns.h"
+#include "forest.h"
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                                                       //
+//          ABANDON                                      //
+//          Armadillo version                            //         
+//                                                       //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::export]]
+Rcpp::List train_forest_root(arma::mat y, arma::mat X, arma::mat Xtest, size_t M, size_t L, size_t N_sweeps, arma::mat max_depth, size_t Nmin, size_t Ncutpoints, double alpha, double beta, double tau, bool draw_sigma, double kap = 16, double s = 4, bool verbose = false, bool m_update_sigma = false, bool draw_mu = false, bool parallel = true){
+
+    size_t N = X.n_rows;
+
+    arma::umat Xorder(X.n_rows, X.n_cols);
+    for(size_t i = 0; i < X.n_cols; i++){
+        Xorder.col(i) = arma::sort_index(X.col(i));
+    }
+
+
+    arma::mat yhats = arma::zeros<arma::mat>(X.n_rows, N_sweeps);
+
+    arma::mat yhats_test = arma::zeros<arma::mat>(Xtest.n_rows, N_sweeps);
+
+    // save predictions of each tree
+    arma::mat predictions(X.n_rows, M);
+
+    // save predictions (based on theta_noise) of each tree
+    // arma::mat predictions_theta_noise(X.n_rows, M);
+
+    arma::mat predictions_test(Xtest.n_rows, M);
+
+    arma::vec yhat = arma::sum(predictions, 1);
+
+    arma::vec yhat_test = arma::zeros<arma::vec>(Xtest.n_rows);
+
+    // current residual
+    arma::mat residual;
+
+    // current residual (based on theta_noise)
+    // arma::vec residual_theta_noise;
+
+    arma::mat sigma_draw(M, N_sweeps);
+
+    double sigma;
+
+    // double tau;
+
+    forest trees(M);
+
+    arma::vec reshat;
+
+    arma::vec reshat_test;
+
+
+    std::vector<double> prob(2, 0.5);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::discrete_distribution<> d(prob.begin(), prob.end());
+    // // sample one index of split point
+    size_t prune;  
+
+
+    // size_t count = 0;
+    for(size_t mc = 0; mc < L; mc ++ ){
+
+        // initialize
+        predictions.fill(arma::as_scalar(arma::mean(y)) / (double) M);
+
+        // predictions_theta_noise.fill(arma::as_scalar(arma::mean(y)) / (double) M);
+
+        predictions_test.fill(arma::as_scalar(arma::mean(y)) / (double) M);
+
+        yhat = arma::sum(predictions, 1);
+
+        yhat_test = arma::sum(predictions_test, 1);
+
+        residual = y - yhat;
+
+
+
+        // residual_theta_noise = y - yhat;
+
+        for(size_t sweeps = 0; sweeps < N_sweeps; sweeps ++){
+
+            // if(verbose == true){
+            // cout << "--------------------------------" << endl;
+            // cout << "number of sweeps " << sweeps << endl;
+            // cout << "--------------------------------" << endl;
+            // }
+
+            for(size_t tree_ind = 0; tree_ind < M; tree_ind ++){
+
+
+                // if update sigma based on residual of all m trees
+                if(m_update_sigma == true){
+                    sigma = 1.0 / sqrt(arma::as_scalar(arma::randg(1, arma::distr_param( (N + kap) / 2.0, 2.0 / as_scalar(sum(pow(residual, 2)) + s)))));
+                    sigma_draw(tree_ind, sweeps) = sigma;
+                }
+
+                // save sigma
+                sigma_draw(tree_ind, sweeps) = sigma;
+
+                // add prediction of current tree back to residual
+                // then it's m - 1 trees residual
+                residual = residual + predictions.col(tree_ind);
+
+                // do the samething for residual_theta_noise, residual of m - 1 trees
+                // residual_theta_noise = residual_theta_noise + predictions_theta_noise.col(tree_ind);
+
+                // prediction of m - 1 trees
+                yhat = yhat - predictions.col(tree_ind);
+
+                // prediction of m - 1 trees on testing set
+                yhat_test = yhat_test - predictions_test.col(tree_ind);
+
+                // grow a tree
+
+                // if(sweeps < 30){
+
+
+                // if(sweeps < 1){
+                    trees.t[tree_ind].grow_tree_adaptive(residual, arma::as_scalar(mean(residual)), Xorder, X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+                    // cout << "tree size " << trees.t[tree_ind].treesize() << endl;
+                // }else{ 
+                //     //    trees.t[tree_ind].sample_theta(residual, X, tau, sigma, draw_mu);
+
+                // // }
+                
+                //     // trees.t[tree_ind].prune_regrow(residual, arma::as_scalar(mean(residual)), X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+                // //     // cout << "tree size, prune and regrow " << trees.t[tree_ind].treesize() << endl;
+                // //     cout << "+++++++++++++++++++++++++++" << endl;
+
+
+                //     prune = d(gen);
+                //     if(prune == 0){
+                //         // cout << " grow " << endl;
+                //         trees.t[tree_ind].one_step_grow(residual, arma::as_scalar(mean(residual)), X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+                //         // cout << "grow, before tree size " << trees.t[tree_ind].treesize() << endl;
+
+                //     }else{
+                //         if(trees.t[tree_ind].treesize() > 5){
+                //         // cout << " prune " << endl;
+                //         trees.t[tree_ind].one_step_prune(residual, arma::as_scalar(mean(residual)), X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+                //         // cout << "prune, before tree size " << trees.t[tree_ind].treesize() << endl;
+                //         }
+                //     }
+
+                // }   
+                // trees.t[tree_ind].grow_tree_adaptive(residual, arma::as_scalar(mean(residual)), Xorder, X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+
+                // trees.t[tree_ind].one_step_prune(residual, arma::as_scalar(mean(residual)), X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel); 
+                // trees.t[tree_ind].one_step_grow(residual, arma::as_scalar(mean(residual)), X, 0, max_depth(tree_ind, sweeps), Nmin, Ncutpoints, tau, sigma, alpha, beta, draw_sigma, draw_mu, parallel);
+          
+
+
+                
+                // cout << "after tree size " << trees.t[tree_ind].treesize() << endl;
+                // cout << "+++++++++++++++++++++++++++" << endl;
+
+
+                if(verbose == true){
+                    cout << "tree " << tree_ind << " size is " << trees.t[tree_ind].treesize() << endl;
+                }
+                
+                // update prediction of current tree
+                predictions.col(tree_ind) = fit_new(trees.t[tree_ind], X);
+
+
+                // cout << "prediction sum of squares " << sum(pow(predictions.col(tree_ind), 2)) << endl;
+
+                // update prediction (theta_noise) of current tree
+                // predictions_theta_noise.col(tree_ind) = fit_new_theta_noise(trees.t[tree_ind], X);
+
+                // update prediction of current tree, test set
+                predictions_test.col(tree_ind) = fit_new(trees.t[tree_ind], Xtest);
+
+                // fit_new_void(trees.t[tree], Xtest, predictions_test, tree);
+
+                // update sigma based on residual of m - 1 trees, residual_theta_noise
+                if(m_update_sigma == false){
+                    sigma = 1.0 / sqrt(arma::as_scalar(arma::randg(1, arma::distr_param( (N + kap) / 2.0, 2.0 / as_scalar(sum(pow(residual, 2)) + s)))));
+                    sigma_draw(tree_ind, sweeps) = sigma;
+                }
+
+                // update residual, now it's residual of m trees
+                residual = residual - predictions.col(tree_ind);
+
+                // residual_theta_noise = residual_theta_noise - predictions_theta_noise.col(tree_ind);
+
+                yhat = yhat + predictions.col(tree_ind);
+
+                yhat_test = yhat_test + predictions_test.col(tree_ind);
+
+            }
+        yhats.col(sweeps) = yhat;
+        yhats_test.col(sweeps) = yhat_test;
+        // cout << "+++++++++++++++++++++++++++++++++++" << endl;
+        // cout << "end of one sweep" << endl;
+        // cout << "+++++++++++++++++++++++++++++++++++" << endl;
+
+        }
+
+    }
+
+    return Rcpp::List::create(Rcpp::Named("yhats") = yhats, Rcpp::Named("yhats_test") = yhats_test, Rcpp::Named("sigma") = sigma_draw);
+}
+
---
+++ src-x64/utility.cpp
@@ -0,0 +1,291 @@
+#include "utility.h"
+
+xinfo copy_xinfo(Rcpp::NumericMatrix& X){
+    size_t n_row = X.nrow();
+    size_t n_col = X.ncol();
+
+    xinfo Xinfo;
+
+    //stacked by column
+    Xinfo.resize(n_col);
+
+    //copy to std matrix
+    for(size_t i = 0; i < n_col; i ++){
+        Xinfo[i].resize(n_row);
+        for(size_t j = 0; j < n_row; j++){
+            Xinfo[i][j] = X(j, i);
+        }
+    }
+
+    // std::move to avoid copying
+    return std::move(Xinfo);
+}
+
+
+xinfo_sizet copy_xinfo_sizet(Rcpp::IntegerMatrix& X){
+    size_t n_row = X.nrow();
+    size_t n_col = X.ncol();
+
+    xinfo_sizet Xinfo;
+
+    //stacked by column
+    Xinfo.resize(n_col);
+
+    //copy to std matrix
+    for(size_t i = 0; i < n_col; i ++){
+        Xinfo[i].resize(n_row);
+        for(size_t j = 0; j < n_row; j++){
+            Xinfo[i][j] = X(j, i);
+        }
+    }
+    // std::move to avoid copying
+    return std::move(Xinfo);
+}
+
+
+void ini_xinfo(xinfo& X, size_t N, size_t p){
+    // xinfo X;
+    X.resize(p);
+
+    for(size_t i = 0; i < p; i ++){
+        X[i].resize(N);
+    }
+
+    // return std::move(X);
+    return;
+}
+
+void ini_xinfo_sizet(xinfo_sizet& X, size_t N, size_t p){
+    // xinfo_sizet X;
+    X.resize(p);
+
+    for(size_t i = 0; i < p; i ++){
+        X[i].resize(N);
+    }
+
+    // return std::move(X);
+    return;
+}
+
+
+double subnode_mean(const std::vector<double>& y, xinfo_sizet& Xorder, const size_t& split_var){
+    // calculate mean of y falls into the same subnode
+    double output = 0.0;
+    size_t N_Xorder = Xorder[split_var].size();
+    for(size_t i = 0; i < N_Xorder; i ++ ){
+        output = output + y[Xorder[split_var][i]];
+    }
+    output = output / N_Xorder;
+    return output;
+}
+
+
+
+void row_sum(xinfo& X, std::vector<double>& output){
+    size_t p = X.size();
+    size_t N = X[0].size();
+    // std::vector<double> output(N);
+    for(size_t i = 0; i < N; i ++){
+        for(size_t j = 0; j < p; j ++ ){
+            // cout << X[j][i] << endl;
+            output[i] = output[i] + X[j][i];
+        }
+    }
+    return;
+}
+
+
+void col_sum(xinfo& X, std::vector<double>& output){
+    size_t p = X.size();
+    size_t N = X[0].size();
+    // std::vector<double> output(p);
+    for(size_t i = 0; i < p; i ++){
+        for(size_t j = 0; j < N; j ++){
+            output[i] = output[i] + X[i][j];
+        }
+    }
+    return;
+}
+
+
+double sum_squared(std::vector<double>& v){
+    size_t N = v.size();
+    double output = 0.0;
+    for(size_t i = 0; i < N; i ++ ){
+        output = output + pow(v[i], 2);
+    }
+    return output;
+}
+
+
+double sum_vec(std::vector<double>& v){
+    size_t N = v.size();
+    double output = 0;
+    for(size_t i = 0; i < N; i ++){
+        output = output + v[i];
+    }
+    return output;
+}
+
+
+void seq_gen(size_t start, size_t end, size_t length_out, arma::uvec& vec){
+    // generate a sequence of INTEGERS
+    double incr = (double) (end - start) / (double) length_out;
+
+    for(size_t i = 0; i < length_out; i ++ ){
+        vec[i] = (size_t) incr * i + start;
+    }
+
+    return;
+}
+
+
+void seq_gen_std(size_t start, size_t end, size_t length_out, std::vector<size_t>& vec){
+    // generate a sequence of integers, save in std vector container
+    double incr = (double)(end - start) / (double)length_out;
+
+    for(size_t i = 0; i < length_out; i ++ ){
+        vec[i] = (size_t) incr * i + start;
+    }
+
+    return;
+}
+
+
+void calculate_y_cumsum(arma::vec& y, double y_sum, arma::uvec& ind, arma::vec& y_cumsum, arma::vec& y_cumsum_inv){
+    // compute cumulative sum of chunks for y, separate by ind vector
+    // y_cumsum_chunk should be lenght M + 1
+    size_t N = y.n_elem;
+    size_t M = y_cumsum.n_elem;
+    assert(y_cumsum.n_elem == y_cumsum_inv.n_elem);
+    size_t ind_ind = 0;
+    arma::vec y_cumsum_chunk(M + 1);
+
+    y_cumsum_chunk[0] = 0; // initialize
+
+    for(size_t i = 0; i < N; i ++ ){
+        if(i <= ind[ind_ind]){
+            y_cumsum_chunk[ind_ind] = y_cumsum_chunk[ind_ind] + y[i];
+        }else{
+              if(ind_ind < M - 1){
+                 ind_ind = ind_ind + 1;
+             }
+            y_cumsum_chunk[ind_ind] = 0;
+            y_cumsum_chunk[ind_ind] = y_cumsum_chunk[ind_ind] + y[i];
+        }
+    }
+
+    y_cumsum[0] = y_cumsum_chunk[0];
+    y_cumsum_inv[0] = y_sum - y_cumsum[0];
+    for(size_t i = 1; i < M; i ++ ){
+        y_cumsum[i] = y_cumsum[i - 1] + y_cumsum_chunk[i];
+        y_cumsum_inv[i] = y_sum - y_cumsum[i];
+    }
+
+    return;
+}
+
+
+
+void calculate_y_cumsum_std(const double * y, const size_t N_y, double y_sum, std::vector<size_t>& ind, std::vector<double>& y_cumsum, std::vector<double>& y_cumsum_inv){
+    // compute cumulative sum of chunks for y, separate by ind vector
+    // N is length of y (total)
+    // y_cumsum_chunk should be lenght M + 1
+    size_t M = y_cumsum.size();
+    assert(y_cumsum.size() == y_cumsum_inv.size());
+    size_t ind_ind = 0;
+    std::vector<double> y_cumsum_chunk(M + 1);
+
+    y_cumsum_chunk[0] = 0; // initialize
+
+    for(size_t i = 0; i < N_y; i ++ ){
+        if(i <= ind[ind_ind]){
+            y_cumsum_chunk[ind_ind] = y_cumsum_chunk[ind_ind] + y[i];
+        }else{
+              if(ind_ind < M - 1){
+                 ind_ind = ind_ind + 1;
+             }
+            y_cumsum_chunk[ind_ind] = 0;
+            y_cumsum_chunk[ind_ind] = y_cumsum_chunk[ind_ind] + y[i];
+        }
+    }
+
+    y_cumsum[0] = y_cumsum_chunk[0];
+    y_cumsum_inv[0] = y_sum - y_cumsum[0];
+    for(size_t i = 1; i < M; i ++ ){
+        y_cumsum[i] = y_cumsum[i - 1] + y_cumsum_chunk[i];
+        y_cumsum_inv[i] = y_sum - y_cumsum[i];
+    }
+
+    return;
+}
+
+void vec_sum(std::vector<double>& vector, double& sum){
+    sum = 0.0;
+    for(size_t i = 0; i < vector.size(); i ++ ){
+        sum = sum + vector[i];
+    }
+    return;
+}
+
+
+double sq_diff_arma_std(arma::vec vec1, std::vector<double> vec2){
+    // compute squared difference between an armadillo vector and a std vector
+    // for debug use
+    assert(vec1.n_elem == vec2.size());
+    size_t N = vec1.n_elem;
+    double output = 0.0;
+    for(size_t i = 0; i < N; i ++ ){
+        output = output + pow(arma::as_scalar(vec1(i)) - vec2[i], 2);
+    }
+    return output;
+}
+
+
+
+double sq_vec_diff(std::vector<double>& v1, std::vector<double>& v2){
+    assert(v1.size() == v2.size());
+    size_t N = v1.size();
+    double output = 0.0;
+    for(size_t i =0; i < N; i ++ ){
+        output = output + pow(v1[i] - v2[i], 2);
+    }
+    return output;
+}
+
+
+std::vector<size_t> sort_indexes(const Rcpp::NumericVector &v) {
+
+  // initialize original index locations
+  std::vector<size_t> idx(v.size());
+  iota(idx.begin(), idx.end(), 0);
+
+  // sort indexes based on comparing values in v
+  sort(idx.begin(), idx.end(),
+       [&v](size_t i1, size_t i2) {return v(i1) < v(i2);});
+
+  return idx;
+}
+
+
+
+void recover_Xorder(xinfo_sizet& Xorder, std::vector<size_t>& Xorder_firstline, xinfo_sizet& Xorder_next_index, xinfo_sizet& Xorder_new){
+    size_t p = Xorder.size();
+    size_t n = Xorder[0].size();
+    size_t current_index;
+    std::vector<size_t> temp;
+
+
+    for(size_t i = 0; i < p; i ++ ){
+        current_index = Xorder_firstline[i];
+        temp.clear();
+        while(current_index >= 0){
+            // cout << Xorder[i][current_index] << endl;
+            temp.push_back(Xorder[i][current_index]);
+            current_index = Xorder_next_index[i][current_index];
+        }
+        Xorder_new[i] = temp;
+        // cout << temp << endl;
+    }
+    return;
+}
