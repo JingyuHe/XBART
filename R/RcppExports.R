dcb4a608f23ca93b62e3c3a0b9bc57035481a8e9
Jingyu He <jingyuhe@MacBook-Pro-6.local>
2019-04-28 09:58:51 -0500

    comment out train_forest_root_std_all

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -10,39 +10,39 @@
 #     .Call(`_XBART_sample_int_ccrank`, n, size, prob)
 # }
 
-train_forest_root_std_all <- function(y, X, Xtest, num_trees, num_sweeps, max_depth, Nmin, num_cutpoints, alpha, beta, tau, no_split_penality = "Auto", burnin = 1L, mtry = 0L, p_categorical = 0L, kap = 16, s = 4, verbose = FALSE, parallel = TRUE, random_seed = NULL, ...) {
+# train_forest_root_std_all <- function(y, X, Xtest, num_trees, num_sweeps, max_depth, Nmin, num_cutpoints, alpha, beta, tau, no_split_penality = "Auto", burnin = 1L, mtry = 0L, p_categorical = 0L, kap = 16, s = 4, verbose = FALSE, parallel = TRUE, random_seed = NULL, ...) {
 
-    if(class(X) != "matrix"){
-        cat("Input X is not a matrix, try to convert type.\n")
-        X = as.matrix(X)
-    }
-    if(class(Xtest) != "matrix"){
-        cat("Input Xtest is not a matrix, try to convert type.\n")
-        Xtest = as.matrix(Xtest)
-    }
-    if(class(y) != "matrix"){
-        cat("Input y is not a matrix, try to convert type.\n")
-        y = as.matrix(y)
-    }
+#     if(class(X) != "matrix"){
+#         cat("Input X is not a matrix, try to convert type.\n")
+#         X = as.matrix(X)
+#     }
+#     if(class(Xtest) != "matrix"){
+#         cat("Input Xtest is not a matrix, try to convert type.\n")
+#         Xtest = as.matrix(Xtest)
+#     }
+#     if(class(y) != "matrix"){
+#         cat("Input y is not a matrix, try to convert type.\n")
+#         y = as.matrix(y)
+#     }
 
-    if(burnin >= num_sweeps){
-        cat("Burnin samples should be smaller than number of sweeps.\n")
-        return();
-    }
+#     if(burnin >= num_sweeps){
+#         cat("Burnin samples should be smaller than number of sweeps.\n")
+#         return();
+#     }
 
-    if(is.null(random_seed)){
-        set_random_seed = FALSE
-        random_seed = 0;
-    }else{
-        cat("Set random seed as ", random_seed, "\n")
-        set_random_seed = TRUE
-    }
-    if(no_split_penality == "Auto"){
-        no_split_penality = log(num_cutpoints)
-    }
+#     if(is.null(random_seed)){
+#         set_random_seed = FALSE
+#         random_seed = 0;
+#     }else{
+#         cat("Set random seed as ", random_seed, "\n")
+#         set_random_seed = TRUE
+#     }
+#     if(no_split_penality == "Auto"){
+#         no_split_penality = log(num_cutpoints)
+#     }
 
-    .Call(`_XBART_train_forest_root_std_all`, y, X, Xtest, num_trees, num_sweeps, max_depth, Nmin, num_cutpoints, alpha, beta, tau, no_split_penality,burnin, mtry, p_categorical, kap, s, verbose, parallel, set_random_seed, random_seed)
-}
+#     .Call(`_XBART_train_forest_root_std_all`, y, X, Xtest, num_trees, num_sweeps, max_depth, Nmin, num_cutpoints, alpha, beta, tau, no_split_penality,burnin, mtry, p_categorical, kap, s, verbose, parallel, set_random_seed, random_seed)
+# }
 
 
 
