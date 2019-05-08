1d2aaafba1c02061632b6857e480c73e310e5038
saaryalov <saar.yalov@gmail.com>
2019-05-07 22:27:53 -0700

    increase python version

--- python/setup.py
+++ python/setup.py
@@ -28,7 +28,7 @@ def readme():
         return f.read()
 
 setup (name = 'xbart',
-       version = '0.1.3',
+       version = '0.1.4',
        author      = "Jingyu He, Saar Yalov, P. Richard Hahn, Lee Reeves",
        description = """XBART project""",
        long_descripition = readme(),
