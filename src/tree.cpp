91765879e4e9d963ced14725ed3322fdde58126c
MaggieWang0614 <maggiewang@asu.edu>
2020-07-17 22:29:51 -0700

    correct loglike size

--- src/tree.cpp
+++ src/tree.cpp
@@ -1779,7 +1779,8 @@ void calculate_likelihood_no_split(std::vector<double> &loglike, size_t &N_Xorde
     for (size_t i = 0; i < loglike.size(); i++){
         if (loglike[i] > -INFINITY){ loglike_size += 1; }
     }
-    loglike[loglike.size() - 1] = model->likelihood(tree_pointer->suff_stat, tree_pointer->suff_stat, loglike.size() - 1, false, true, state) + log(pow(1.0 + tree_pointer->getdepth(), model->beta) / model->alpha - 1.0) + log((double)loglike_size - 1.0) + log(model->getNoSplitPenality());
+    loglike[loglike.size() - 1] = model->likelihood(tree_pointer->suff_stat, tree_pointer->suff_stat, loglike.size() - 1, false, true, state) + log(pow(1.0 + tree_pointer->getdepth(), model->beta) / model->alpha - 1.0) + log((double)loglike_size) + log(model->getNoSplitPenality()); 
+    // !!Note loglike_size shouldn't get minus 1 when it count non zero of loglike.
   
     // loglike[loglike.size() - 1] = model->likelihood(tree_pointer->suff_stat, tree_pointer->suff_stat, loglike.size() - 1, false, true, state) + log(pow(1.0 + tree_pointer->getdepth(), model->beta) / model->alpha - 1.0) + log((double)loglike.size() - 1.0) + log(model->getNoSplitPenality());
   
