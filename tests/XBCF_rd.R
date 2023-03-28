f16084b060979dca4c70f40078a428c21252f17f
MaggieWang0614 <maggiew614@gmail.com>
2023-03-28 10:53:33 -0700

    clean up while loop

--- tests/XBCF_rd.R
+++ tests/XBCF_rd.R
@@ -40,7 +40,7 @@ data <- list(y = ytest, W = wtest, X = xtest, c = c, Wtr = w, Xtr = x)
 tau.prior = var(y) / (fit.XBCFrd$model_params$n_trees_con = fit.XBCFrd$model_params$n_trees_mod)
 
 pred.XBCFrd <- predict.XBCFrd(fit.XBCFrd, W = wtest, X = xtest)
-pred.XBCFrdgp <- predict.XBCFrdgp(fit.XBCFrd, W = wtest, X = xtest, Wtr = w, Xtr = x, theta = 1, tau = 0.01)
+pred.XBCFrdgp <- predict.XBCFrdgp(fit.XBCFrd, W = wtest, X = xtest, Wtr = w, Xtr = x, theta = 1, tau = 0.00001)
 
 # Check yhats
 rmse.yhats <- sqrt(mean((data$y - pred.XBCFrd$yhats.adj.mean)^2))
@@ -48,23 +48,28 @@ print(paste("RMSE on yhats ", round(rmse.yhats, 3), sep = ""))
 
 # Check tauhats in the test bandwidth
 test.ind <- (xtest <= c+h_test) & (xtest >= c-h_test)
-ate.hat <- colMeans(pred.XBCFrd$tau.adj[test.ind, ])[(burnin + 1):num_sweeps]
 expected_ate <- mean(tau.test[test.ind])
+ate.hat <- colMeans(pred.XBCFrd$tau.adj[test.ind, ])[(burnin + 1):num_sweeps]
 rmse.ate <- sqrt(mean((ate.hat - expected_ate)^2))
-print(paste("RMSE on ATE ", round(rmse.ate, 3), sep = ""))
+print(paste("XBCF RMSE on ATE ", round(rmse.ate, 3), sep = ""))
+
+ate.hat.gp <- colMeans(pred.XBCFrdgp$tau.adj[test.ind, ])[(burnin + 1):num_sweeps]
+rmse.ate.gp <- sqrt(mean((ate.hat.gp - expected_ate)^2))
+print(paste("XBCF-GP RMSE on ATE ", round(rmse.ate.gp, 3), sep = ""))
 
 
 par(mfrow = c(1, 2))
 tau.hat <- pred.XBCFrd$tau.adj.mean
-plot(xtest, tau.test, ylim = range(tau.test, tau.hat))
+tau.hat.gp <- pred.XBCFrdgp$tau.adj.mean
+plot(xtest, tau.test, ylim = range(tau.test, tau.hat, tau.hat.gp), main = 'XBCF')
 points(xtest, tau.hat, col = 'blue')
-# legend("topleft", legend = c("True", "XBCF-GP"), col = c("black", "blue"), pch = 1)
+legend("topleft", legend = c("True", "Estimate"), col = c("black", "blue"), pch = 1)
+
+plot(xtest, tau.test, ylim = range(tau.test, tau.hat.gp, tau.hat), main = 'XBCF-GP')
+points(xtest, tau.hat.gp, col = 'blue')
 
 # y.hat <- pred.XBCFrd$yhats.adj.mean
 # plot(xtest, ytest, ylim = range(ytest, y.hat))
 # points(xtest, y.hat, col = 'blue')
 # legend("topleft", legend = c("True", "XBCF-GP"), col = c("black", "blue"), pch = 1)
 
-tau.hat.gp <- pred.XBCFrdgp$tau.adj.mean
-plot(xtest, tau.test, ylim = range(tau.test, tau.hat))
-points(xtest, tau.hat.gp, col = 'blue')
